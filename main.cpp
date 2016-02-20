13534c298943ac5b6b580f6e4374b156632f2ae1
lexa <lexako1@mail.ru>
2016-02-21 00:23:44 +0500

    added paths to run on windows

--- main.cpp
+++ main.cpp
@@ -3,10 +3,29 @@
 
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFileInfo>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
+    QString appPath = QFileInfo(argv[0]).dir().path();
+
+    QStringList paths = QCoreApplication::libraryPaths();
+    qDebug() << paths;
+    paths.append(".");
+    paths.append(appPath);
+    paths.append(appPath + "/platforms");
+    //paths.append(appPath + "/platform");
+    QCoreApplication::setLibraryPaths(paths);
+    qDebug() << paths;
+
+
     QApplication a(argc, argv);
+
+    a.setOrganizationName("Alexey Tchernopyatov");
+    a.setOrganizationDomain("tchernopyatov.com");
+    a.setApplicationName("Cycling Workout");
+
     MainWindow w;
     w.show();
 
